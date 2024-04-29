3308857a00cbdfc2f0b06ddf65f933b865416df8
mananvirmani611 <mananvirmani611@gmail.com>
2024-04-29 21:16:12 +0530

    database connected to mongo cluster

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const CONSTANTS = {
     DATABASE : {
         DATABASE_CONNECTION_ERROR : "Not able to connect to database",
         DATABASE_CONNECTION_SUCCESS : "Database succesfully connected",
-        DATABASE_CONNECTION_URL : "mongodb://127.0.0.1:27017/",
+        DATABASE_CONNECTION_URL : "mongodb+srv://mananvirmani611:" + process.env.DB_PASSWORD + "@StockMarketLearning.vvlm6fx.mongodb.net/",
         DATABASE_NAME : "stock_market_learning"
     },
     THIRD_PARTY_URL : {
